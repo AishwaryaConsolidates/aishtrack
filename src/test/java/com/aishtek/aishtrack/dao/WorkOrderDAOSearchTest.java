becc99d06a8018b89f93d201010ca717717a4bea
Adarsh Devanhalli <adarshadarsh@gmail.com>
2020-10-23 04:58:24 -0500

    work order search test

--- src/test/java/com/aishtek/aishtrack/dao/WorkOrderDAOSearchTest.java
+++ src/test/java/com/aishtek/aishtrack/dao/WorkOrderDAOSearchTest.java
@@ -1,115 +1,76 @@
 package com.aishtek.aishtrack.dao;
 
+import static org.junit.Assert.assertEquals;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.Test;
 import com.aishtek.aishtrack.test.BaseIntegrationTest;
+import com.aishtek.aishtrack.utils.WorkStatus;
 
 public class WorkOrderDAOSearchTest extends BaseIntegrationTest {
 
-  private int customerId;
-  private String type = "service report";
-  private String notes = "This is a note";
+  @Test
+  public void searchSearchesByCustomerNameNickName() throws SQLException {
+    try (Connection connection = getConnection()) {
+      int customerId = createTestCustomer(connection);
+      int personId = createTestPerson(connection);
 
-  // @Test
-  // public void searchSearchesByCustomerNameNickName() throws SQLException {
-  // try (Connection connection = getConnection()) {
-  // customerId = createTestCustomer(connection);
-  // // WorkOrder workOrder = new WorkOrder(customerId, type, notes);
-  // // WorkOrderDAO.create(connection, workOrder);
-  // //
-  // // ArrayList<WorkOrder> workOrders = WorkOrderDAO.searchFor(connection, "Bajji", 0, null);
-  // //
-  // // assertEquals(workOrders.size(), 1);
-  // // assertEquals(workOrders.get(0).getCustomerId(), customerId);
-  // // assertEquals(workOrders.get(0).getCustomerName(), "Bajji Corner");
-  // // assertEquals(workOrder.getNotes(), notes);
-  // // assertEquals(workOrder.getType(), type);
-  // // assertEquals(workOrder.getStatus(), WorkStatus.CREATED_STATUS);
-  //
-  // connection.rollback();
-  // } catch (SQLException e) {
-  // System.out.println(e);
-  // assert (false);
-  // }
-  // }
-  //
-  // @Test
-  // public void searchDoesAPartialMatch() throws SQLException {
-  // try (Connection connection = getConnection()) {
-  // customerId = createTestCustomer(connection);
-  // WorkOrder workOrder = new WorkOrder(customerId, type, notes);
-  // WorkOrderDAO.create(connection, workOrder);
-  //
-  // ArrayList<WorkOrder> workOrders = WorkOrderDAO.searchFor(connection, "ajj", 0, null);
-  //
-  // assertEquals(workOrders.size(), 1);
-  // assertEquals(workOrders.get(0).getCustomerId(), customerId);
-  // assertEquals(workOrders.get(0).getCustomerName(), "Bajji Corner");
-  // assertEquals(workOrder.getNotes(), notes);
-  // assertEquals(workOrder.getType(), type);
-  // assertEquals(workOrder.getStatus(), WorkStatus.CREATED_STATUS);
-  //
-  // connection.rollback();
-  // } catch (SQLException e) {
-  // System.out.println(e);
-  // assert (false);
-  // }
-  // }
-  //
-  // @Test
-  // public void searchSearchesByStatus() throws SQLException {
-  // try (Connection connection = getConnection()) {
-  // customerId = createTestCustomer(connection);
-  // WorkOrder workOrder = new WorkOrder(customerId, type, notes);
-  // WorkOrderDAO.create(connection, workOrder);
-  //
-  // String[] statuses = {WorkStatus.CREATED_STATUS, WorkStatus.COMPLETED_STATUS};
-  // ArrayList<WorkOrder> workOrders =
-  // WorkOrderDAO.searchFor(connection, "ajj", 0, statuses);
-  //
-  // assertEquals(workOrders.size(), 1);
-  // assertEquals(workOrder.getStatus(), WorkStatus.CREATED_STATUS);
-  //
-  // connection.rollback();
-  // } catch (SQLException e) {
-  // System.out.println(e);
-  // assert (false);
-  // }
-  // }
-  //
-  // @Test
-  // public void searchDoesNotReturnIfDifferentStatus() throws SQLException {
-  // try (Connection connection = getConnection()) {
-  // customerId = createTestCustomer(connection);
-  // WorkOrder workOrder = new WorkOrder(customerId, type, notes);
-  // WorkOrderDAO.create(connection, workOrder);
-  //
-  // String[] statuses = {WorkStatus.ASSIGNED_STATUS};
-  // ArrayList<WorkOrder> workOrders =
-  // WorkOrderDAO.searchFor(connection, "ajj", 0, statuses);
-  //
-  // assertEquals(workOrders.size(), 0);
-  //
-  // connection.rollback();
-  // } catch (SQLException e) {
-  // System.out.println(e);
-  // assert (false);
-  // }
-  // }
-  //
-  // @Test
-  // public void searchDoesNotReturnIfNameDoesntMatch() throws SQLException {
-  // try (Connection connection = getConnection()) {
-  // customerId = createTestCustomer(connection);
-  // WorkOrder workOrder = new WorkOrder(customerId, type, notes);
-  // WorkOrderDAO.create(connection, workOrder);
-  //
-  // ArrayList<WorkOrder> workOrders = WorkOrderDAO.searchFor(connection, "Burgi", 0, null);
-  //
-  // assertEquals(workOrders.size(), 0);
-  //
-  // connection.rollback();
-  // } catch (SQLException e) {
-  // System.out.println(e);
-  // assert (false);
-  // }
-  // }
+      int workOrderId = createTestWorkOrder(connection, customerId, personId);
+
+      ArrayList<HashMap<String, String>> workOrders =
+          WorkOrderDAO.searchFor(connection, "Bajji", 0, null);
+
+      assertEquals(workOrders.size(), 1);
+      assertEquals(workOrders.get(0).get("id"), "" + workOrderId);
+
+      connection.rollback();
+    } catch (SQLException e) {
+      System.out.println(e);
+      assert (false);
+    }
+  }
+
+  @Test
+  public void searchSearchesByCustomerId() throws SQLException {
+    try (Connection connection = getConnection()) {
+      int customerId = createTestCustomer(connection);
+      int personId = createTestPerson(connection);
+
+      int workOrderId = createTestWorkOrder(connection, customerId, personId);
+
+      ArrayList<HashMap<String, String>> workOrders =
+          WorkOrderDAO.searchFor(connection, null, customerId, null);
+
+      assertEquals(workOrders.size(), 1);
+      assertEquals(workOrders.get(0).get("id"), "" + workOrderId);
+
+      connection.rollback();
+    } catch (SQLException e) {
+      System.out.println(e);
+      assert (false);
+    }
+  }
+
+  @Test
+  public void searchSearchesByStatus() throws SQLException {
+    try (Connection connection = getConnection()) {
+      int customerId = createTestCustomer(connection);
+      int personId = createTestPerson(connection);
+
+      int workOrderId = createTestWorkOrder(connection, customerId, personId);
+      String[] searchStatuses = {WorkStatus.CREATED_STATUS};
+      ArrayList<HashMap<String, String>> workOrders =
+          WorkOrderDAO.searchFor(connection, null, 0, searchStatuses);
+
+      assertEquals(workOrders.size(), 1);
+      assertEquals(workOrders.get(0).get("id"), "" + workOrderId);
+
+      connection.rollback();
+    } catch (SQLException e) {
+      System.out.println(e);
+      assert (false);
+    }
+  }
 }
